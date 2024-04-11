041c397d097de4165e7017a3b9d25922a46967c3
BaoHG <baohuynh291@gmail.com>
2024-04-11 12:08:57 +0700

    add
    add

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter size of array: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] array = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.Write("Array: ");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+        //add element
+        Console.WriteLine("Add element: ");
+        int newelement = Convert.ToInt32(Console.ReadLine());
+        int[] newarray = new int[n + 1];
+        for (int i = 0; i < n; i++)
+        {
+            newarray[i] = array[i];
+        }
+        newarray[n] = newelement;
+        Console.Write("New Array: ");
+        foreach (int element in newarray)
+        {
+            Console.Write(element);
+        }
+    }
+}
